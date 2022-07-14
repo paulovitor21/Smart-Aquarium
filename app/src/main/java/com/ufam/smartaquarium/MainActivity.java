7d430d10400f1eaf3e62a155915cea3c83991ef9
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2022-07-14 16:46:40 -0400

    menu de navegação

--- app/src/main/java/com/ufam/smartaquarium/MainActivity.java
+++ app/src/main/java/com/ufam/smartaquarium/MainActivity.java
@@ -1,14 +1,44 @@
 package com.ufam.smartaquarium;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.ufam.smartaquarium.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.homeMenu: replaceFragment(new HomeFragment());
+                    break;
+                case R.id.dashboardMenu: replaceFragment(new DashboardFragment());
+                    break;
+                case R.id.notificationMenu: replaceFragment(new NotificationFragment());
+                    break;
+                case R.id.profileMenu: replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
